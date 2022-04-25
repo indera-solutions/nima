7a27f4cd4b6c3d5af5c2fa6ca8d1f2581939f294
JohnBelly95 <johnbelly95@gmail.com>
2022-04-25 12:57:45 +0300

    chore(api): More Entities Ported

--- apps/api/src/products/entities/productVariant.entity.ts
+++ apps/api/src/products/entities/productVariant.entity.ts
@@ -1,6 +1,65 @@
-import { Entity } from 'typeorm';
+import { Metadata, Product, ProductVariant, Translatable } from '@nima/interfaces';
+import { Column, CreateDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+import { ProductEntity } from './product.entity';
 
 @Entity()
-export class ProductVariantEntity{
+export class ProductVariantEntity implements ProductVariant {
+	@PrimaryGeneratedColumn()
+	id: number;
 
+	@Column()
+	sku: string;
+
+	@Column({ type: 'jsonb', default: {} })
+	name: Translatable;
+
+	@ManyToOne(() => ProductEntity)
+	product: Product;
+
+	@Column({ nullable: true })
+	weight?: number;
+
+	@Column({ type: 'jsonb', default: {} })
+	metadata: Metadata;
+
+	@Column({ type: 'jsonb', default: {} })
+	privateMetadata: Metadata;
+
+	@Column({ nullable: true })
+	sortOrder?: number;
+
+	@Column({ nullable: true })
+	isPreorder?: boolean;
+
+	@Column({ nullable: true })
+	preorderEndDate?: string;
+
+	@Column({ nullable: true })
+	preorderGlobalThreshold?: number;
+
+	@Column({ nullable: true })
+	quantityLimitPerCustomer?: number;
+
+	@CreateDateColumn({ type: String })
+	created: string;
+
+	@UpdateDateColumn({ type: String })
+	updatedAt: string;
+
+	//Pricing
+	@Column()
+	currency: string;
+
+	@Column({ nullable: true })
+	priceAmount?: number;
+
+	@Column({ nullable: true })
+	costPriceAmount?: number;
+
+	//Stock
+	@Column()
+	stock: number;
+
+	@Column()
+	trackInventory: boolean;
 }
